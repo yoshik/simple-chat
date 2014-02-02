8c80851c08b7b604fd31c68d7e78a4b0deb0781f
yoshik <fujiwara.access@gmail.com>
2014-02-03 02:11:44 +0900

    viewcontroller update

--- app/assets/javascript/viewcontroller.js
+++ app/assets/javascript/viewcontroller.js
@@ -1,13 +1,36 @@
 Zepto(function($){
-  var index = Hogan.compile($('#index').text());
-  var signup = Hogan.compile($('#signup').text());
-  $('#view').html(index.render());
 
-  $("#screen-index").show();
 
-  $("#btn-index-signup").on("click",function(){
-    var signup = Hogan.compile($('#signup').text());
-    $('#view').html(signup.render());
-  });
+  var template={};
+  var view={};
+
+  view.index=function(){
+    if(!template.index){
+      template.index = Hogan.compile($('#index').text());
+    }
+    $('#view').html(template.index.render());
+    $("#index-signup").on("click",function(){view.signup();});
+    $("#index-signin").on("click",function(){view.signin();});
+  };
+
+  view.signup=function(){
+    if(!template.signup){
+      template.signup = Hogan.compile($('#signup').text());
+    }
+    $('#view').html(template.signup.render());
+    $("#signup-submit").on("click",function(){view.index();});
+    $("#signup-cancel").on("click",function(){view.index();});
+  };
+
+  view.signin=function(){
+    if(!template.signin){
+      template.signin = Hogan.compile($('#signin').text());
+    }
+    $('#view').html(template.signin.render());
+    $("#signin-submit").on("click",function(){view.index();});
+    $("#signin-cancel").on("click",function(){view.index();});
+  };
+
+  view.index();
 
 });
