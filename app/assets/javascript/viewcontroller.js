722f45d82c8b0341509131fdd98250dcc3cfa25f
yoshik <fujiwara.access@gmail.com>
2014-02-03 05:02:53 +0900

    update: viewcontroller

--- app/assets/javascript/viewcontroller.js
+++ app/assets/javascript/viewcontroller.js
@@ -1,5 +1,6 @@
-Zepto(function($){
+var user={};
 
+Zepto(function($){
 
   var template={};
   var view={};
@@ -18,7 +19,26 @@ Zepto(function($){
       template.signup = Hogan.compile($('#signup').text());
     }
     $('#view').html(template.signup.render());
-    $("#signup-submit").on("click",function(){view.index();});
+    $("#signup-submit").on("click",function(){
+      data={};
+      data.username=$('#signup-username').val();
+      data.password=$('#signup-password').val();
+      $.ajax({
+        url: "/registration",
+        type: "POST",
+        cache: false,
+        contentType: "application/json; charset=UTF-8",
+        data: JSON.stringify(data),
+        success: function(message) {
+          $("#signup-result").text(message.ok);
+          user.name=data.username;
+          user.pass=data.password;
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown){
+          $("#signup-result").text(JSON.parse(XMLHttpRequest.responseText).error);
+        }
+      });
+    });
     $("#signup-cancel").on("click",function(){view.index();});
   };
 
@@ -27,7 +47,26 @@ Zepto(function($){
       template.signin = Hogan.compile($('#signin').text());
     }
     $('#view').html(template.signin.render());
-    $("#signin-submit").on("click",function(){view.index();});
+    $("#signin-submit").on("click",function(){
+      data={};
+      data.username=$('#signin-username').val();
+      data.password=$('#signin-password').val();
+      $.ajax({
+        url: "/registration",
+        type: "POST",
+        cache: false,
+        contentType: "application/json; charset=UTF-8",
+        data: JSON.stringify(data),
+        success: function(message) {
+          $("#signin-result").text(message.ok);
+          user.name=data.username;
+          user.pass=data.password;
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown){
+          $("#signin-result").text(JSON.parse(XMLHttpRequest.responseText).error);
+        }
+      });
+    });
     $("#signin-cancel").on("click",function(){view.index();});
   };
 
